45b47f400e50729e5422c66a9297c82f2ab3dde9
Sam-Smyth <smyth.j.sam@gmail.com>
2019-08-25 13:54:59 -0700

    Added to-do list. Styled. Attempted check box

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-let sliderOutput = document.querySelector('.range-slider-output');
+let sliderOutput = document.querySelector('.range-slider--value');
 
-document.querySelector('.range-slider').addEventListener('input', function(event) {
+document.querySelector('.range-slider--input').addEventListener('input', function(event) {
   sliderOutput.innerHTML = event.currentTarget.value;
 });
+
+var todoInput = document.querySelector('.todo--input');
+var todoSubmit = document.querySelector('.todo--submit');
+var todoList = document.querySelector('.todo--list');
+var todoCheck = document.querySelector('.todo--list li');
+
+console.log(todoCheck);
+
+function createToDo() {
+  if (todoInput.value === "") {
+    return;
+  }
+
+  todoList.insertAdjacentHTML("beforeend", "<li>" + todoInput.value + "</li>")
+  
+  var newListItem = todoList.lastChild;
+  
+  newListItem.addEventListener("click", function () {
+    // todoList.removeChild(newListItem);
+    todoCheck.classList.add('todo--list--checked');
+  })
+  
+  todoInput.value = "";
+}
+
+todoSubmit.addEventListener("click", function () {
+  createToDo();
+})
+
+todoInput.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    createToDo();
+  }
+})
